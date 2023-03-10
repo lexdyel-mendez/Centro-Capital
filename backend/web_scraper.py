bf1a884c8ba7c1e3c6326b259593bd60a45efa1b
Adean Colon <aecf98@gmail.com>
2023-03-10 14:33:05 -0400

    Refactor beautifulSoup setup for readability and usability

--- backend/web_scraper.py
+++ backend/web_scraper.py
@@ -3,27 +3,43 @@ from urllib.request import Request, urlopen
 import requests
 import re
 
-url = "https://tradingeconomics.com/puerto-rico/gdp"
+link = "https://tradingeconomics.com/puerto-rico/gdp"
+html_file = "Puerto Rico GDP - 2022 Data - 2023 Forecast - 1960-2021 Historical - Chart - News.html"
 
-# result = requests.get(url)
+
+def request_html_tree(url):
+    # result = requests.get(url)
+
+    # in case of 403 Forbidden error. Must read how to handle robots.txt
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    return BeautifulSoup(webpage, "html.parser")
 
 
-req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
+def get_html_tree_from_file(file):
+    with open(file, "r") as f:
+        html_tree = BeautifulSoup(f, "html.parser")
+    return html_tree
 
-webpage = urlopen(req).read()
-doc = BeautifulSoup(webpage, "html.parser")
-# title = page_soup.find("title")
-# tables = page_soup.find("table")
+# example of finding the title tag
+# title = html_tree.find("title")
 
-# with open("Puerto Rico GDP - 2022 Data - 2023 Forecast - 1960-2021 Historical - Chart - News.html", "r") as f:
-#     doc = BeautifulSoup(f, "html.parser")
+# example of finding the FIRST table in html
+# tables = html_tree.find("table")
 
-tables = doc.find_all("table")
-# text = doc.get_text()
+# example of finding all tables in html
+# tables = html_tree.find_all("table")
+
+# example of getting all the text from html
+# text = html_tree.get_text()
+
+# example of finding value from a table after executing find_all(table)
 # table_data = tables[0].find_all(["td"], text="106.53")
 # gdp_value = table_data[1].string
-#
+
+# Attempting to find data with REGEX
 # regex_var = doc.find_all(text=re.compile(""))
 
-trs = tables[0].contents
-tags = list(trs[0].next_sibling.next_sibling.next_sibling.descendants)
+# example of traversing tree
+# trs = tables[0].contents
+# tags = list(trs[0].next_sibling.next_sibling.next_sibling.descendants)
