2b34a50d124e65f8af20d3c284e94999872ad6ec
Adean Colon <aecf98@gmail.com>
2023-03-29 14:12:05 -0400

    Fix merge issues
    Signed-off-by: Adean Colon <aecf98@gmail.com>

--- frontend/centro-capital-frontend/src/App.js
+++ frontend/centro-capital-frontend/src/App.js
@@ -2,16 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@coreui/coreui/dist/css/coreui.min.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NoPage from "./pages/NoPage";
 import Insights from './pages/Insights';
 import Compare from './pages/Compare';
-
-// Importing modules
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
@@ -23,46 +21,13 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="insights" element={<Insights />} />
           <Route path="compare" element={<Compare />} />
+            <Route path="centro-capital" element={<Welcome/>}/>
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
     </div>
   );
-    // usestate for setting a javascript
-    // object for storing and using msg
-    const [msg, setmsg] = useState({});
-
-    // Using useEffect for single rendering
-    useEffect(() => {
-        // Using fetch to fetch the api from
-        // flask server it will be redirected to proxy
-        fetch("/centro-capital").then((res) =>
-            res.json().then((data) => {
-                // Setting a msg from api
-                setmsg({
-                    msg: data.Name,
-                    age: data.Age,
-                    date: data.Date,
-                    programming: data.programming,
-                });
-            })
-        );
-    }, []);
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>React and flask</h1>
-                {/* Calling a msg from setdata for showing */}
-                <p>{msg.name}</p>
-                <p>{msg.age}</p>
-                <p>{msg.date}</p>
-                <p>{msg.programming}</p>
-
-            </header>
-        </div>
-    );
 }
 
 export default App;
--- frontend/centro-capital-frontend/src/pages/Welcome.js
+++ frontend/centro-capital-frontend/src/pages/Welcome.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 function Welcome() {
 
+    // usestate for setting a javascript
+    // object for storing and using data
     const [data,setData] = useState([{}])
 
+    // Using useEffect for single rendering
     useEffect(() => {
+        // Using fetch to fetch the api from
+        // flask server it will be redirected to proxy
         fetch("/centro-capital").then(
             res => res.json()
         ).then(
