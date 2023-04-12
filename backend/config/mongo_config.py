1b72a5847e540b23122a21845822e12495be475e
Lexdyel Mendez Rios <lexdyel.mendez@upr.edu>
2023-04-12 12:54:06 -0400

    Heroku Config Vars
    Added so that the configurations for loggin into the DB uses the heroku variables instead of specified one.
    
    Signed-off-by: Lexdyel Mendez Rios <lexdyel.mendez@upr.edu>

--- backend/config/mongo_config.py
+++ backend/config/mongo_config.py
@@ -1,7 +1,13 @@
 #Database configuration settings
+import os
+# mongo_config = {
+#     'user':'lexdyel-mendez',
+#     'passwrd':'transitdev08',
+#     'dbname':"centro_capital_dev"
+# }
 
 mongo_config = {
-    'user':'lexdyel-mendez',
-    'passwrd':'transitdev08',
-    'dbname':"centro_capital_dev"
+    'user':os.getenv("MONGO_USER"),
+    'passwrd':os.getenv("MONGO_PASS"),
+    'dbname':os.getenv('MONGO_DBNAME')
 }
