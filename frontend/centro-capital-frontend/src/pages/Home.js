ebd90f2f293f747b8cd376cae7c0521387f133f6
juandiaz28 <juan.diaz28@upr.edu>
2023-04-25 10:59:31 -0400

    Added area chart and pie chart with input

--- frontend/centro-capital-frontend/src/pages/Home.js
+++ frontend/centro-capital-frontend/src/pages/Home.js
@@ -4,6 +4,105 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import CustomBar from "../components/CustomBar";
 import CustomLine from '../components/CustomLine'
+import CustomPie from '../components/CustomPie';
+import CustomArea from '../components/CustomArea';
+const areadata = [
+  {
+    "name": "Page A",
+    "uv": 4000,
+    "pv": 2400,
+    "amt": 2400
+  },
+  {
+    "name": "Page B",
+    "uv": 3000,
+    "pv": 1398,
+    "amt": 2210
+  },
+  {
+    "name": "Page C",
+    "uv": 2000,
+    "pv": 9800,
+    "amt": 2290
+  },
+  {
+    "name": "Page D",
+    "uv": 2780,
+    "pv": 3908,
+    "amt": 2000
+  },
+  {
+    "name": "Page E",
+    "uv": 1890,
+    "pv": 4800,
+    "amt": 2181
+  },
+  {
+    "name": "Page F",
+    "uv": 2390,
+    "pv": 3800,
+    "amt": 2500
+  },
+  {
+    "name": "Page G",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  }
+]
+
+const data01 = [
+  {
+    "name": "Group A",
+    "value": 400
+  },
+  {
+    "name": "Group B",
+    "value": 300
+  },
+  {
+    "name": "Group C",
+    "value": 300
+  },
+  {
+    "name": "Group D",
+    "value": 200
+  },
+  {
+    "name": "Group E",
+    "value": 278
+  },
+  {
+    "name": "Group F",
+    "value": 189
+  }
+];
+const data02 = [
+  {
+    "name": "Group A",
+    "value": 2400
+  },
+  {
+    "name": "Group B",
+    "value": 4567
+  },
+  {
+    "name": "Group C",
+    "value": 1398
+  },
+  {
+    "name": "Group D",
+    "value": 9800
+  },
+  {
+    "name": "Group E",
+    "value": 3908
+  },
+  {
+    "name": "Group F",
+    "value": 4800
+  }
+];
 const input = [
   {
     name: '2014',
@@ -38,8 +137,8 @@ const Home = () => {
         <Col className="bg-secondary m-4 rounded"><CustomLine data={input}></CustomLine></Col>
       </Row>
       <Row className="m-4">
-        <Col className="bg-warning m-4 rounded"><CustomLine data={input}></CustomLine></Col>
-        <Col className="bg-info m-4 rounded"> filler</Col>
+        <Col className="bg-warning m-4 rounded"><CustomArea data={areadata}></CustomArea></Col>
+        <Col className="bg-info m-4 rounded"> <CustomPie data01={data01} data02={data02}></CustomPie></Col>
       </Row>
     </Container>
       // <div>
