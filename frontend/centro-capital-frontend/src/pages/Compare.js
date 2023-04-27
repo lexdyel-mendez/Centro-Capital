a3bfe4c4391bdc93d7a2949cdae225c969a69603
Lexdyel Mendez Rios <lexdyel.mendez@upr.edu>
2023-04-27 13:30:25 -0400

    Testing Fetched data
    Testing that the fetched data is being presented correctly in the front end.
    
    Signed-off-by: Lexdyel Mendez Rios <lexdyel.mendez@upr.edu>

--- frontend/centro-capital-frontend/src/pages/Compare.js
+++ frontend/centro-capital-frontend/src/pages/Compare.js
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from "react";
 
+function organizeData(inputData){
+  //data filter after extraction START
+  const finalValArr=[]
+  for(const key of Object.keys(inputData).slice(0,9)){
+    const currYears = new Object()
+    currYears['year'] = key
+    for(const [innerKey, innerValue] of Object.entries(inputData[key])){
+      currYears[innerKey] = innerValue
+    }
+  }
+  //data filter after extraction END
+  return  finalValArr
+}
 
 const Compare = () => {
 
@@ -13,15 +26,18 @@ const Compare = () => {
       }
     }).then(data => setdata(data))
     .then(error => console.log(error))
-  })
+  },[])
+
 
   console.log(data)
   console.log(typeof data)
 
+  console.log(organizeData(data))
+
 
   return (
     <div>
-      <p>hi</p>
+    <p>x</p>
     </div>
   )
 
