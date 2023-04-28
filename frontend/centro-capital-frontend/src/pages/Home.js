5c98a74b690f61bbf672d13c56bed1afe48f760a
juandiaz28 <juan.diaz28@upr.edu>
2023-04-28 16:21:40 -0400

    Removed npm axios and react-query

--- frontend/centro-capital-frontend/src/pages/Home.js
+++ frontend/centro-capital-frontend/src/pages/Home.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useQueries} from '@tanstack/react-query';
-import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
