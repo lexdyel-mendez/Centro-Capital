0528c47c4f784aafbfdb6edaf085a35d7b729b86
juandiaz28 <juan.diaz28@upr.edu>
2023-05-01 15:19:59 -0400

    Added google form for feedback

--- frontend/centro-capital-frontend/src/pages/Welcome.js
+++ frontend/centro-capital-frontend/src/pages/Welcome.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function Welcome() {
 
     // usestate for setting a javascript
@@ -24,7 +26,15 @@ function Welcome() {
 
     return (
         <div>
-            <p>{data.welcome}</p>
+        <Container>
+            <Row className="m-4 d-flex align-items-center justify-content-center">
+                <Col className=" d-flex align-items-center justify-content-center"><h2>Centro Capital is a student project that intends to promote the knowledge of well-being in Puerto Rico</h2></Col>
+                <Col className=" d-flex align-items-center justify-content-center"><h4>Feedback is a quintessential part of growing so please let us know your thoughts</h4></Col>
+                </Row>
+               <h6>Feel free to contact us at: juan.diaz28@upr, adean.colon@upr.edu, or lexdyel.mendez@upr.edu</h6>
+        
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdreUsik8fmSbtcpQJNR74Xyxj_KGa6kD4TJ_94gQHemmo92Q/viewform?embedded=true" width="640" height="1940" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </Container>
         </div>
     )
 }
