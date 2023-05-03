81e1c1a4ff31872465ca5b1ee0d6614ae2c2d502
juandiaz28 <juan.diaz28@upr.edu>
2023-05-03 15:47:56 -0400

    run build

--- frontend/centro-capital-frontend/src/App.js
+++ frontend/centro-capital-frontend/src/App.js
@@ -19,7 +19,6 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <AnalyticsHandler />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
