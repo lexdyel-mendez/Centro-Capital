93048457b8b4104fdbc4cb7a3081cce9ffb266bb
Adean Colon <aecf98@gmail.com>
2023-05-02 20:27:48 -0400

    remove react-ga packaged and add google tag

--- frontend/centro-capital-frontend/src/App.js
+++ frontend/centro-capital-frontend/src/App.js
@@ -9,22 +9,7 @@ import NoPage from "./pages/NoPage";
 import Insights from './pages/Insights';
 import Compare from './pages/Compare';
 import Welcome from "./pages/Welcome";
-import ReactGA from 'react-ga4'
 import React from 'react';
-
-ReactGA.initialize('G-Q8GRTCK6RZ');
-// ReactGA.send({
-//   hitType: 'timing',
-//   timingCategory: 'Engagement',
-//   timingVar: 'Form Submission',
-//   timingValue: 20000
-// });
-
-// console.log('Timing event sent:', {
-//   timingCategory: 'Engagement',
-//   timingVar: 'Form Submission',
-//   timingValue: 20000
-// });
 
 function App() {
   return (
