c5d28b43acdc5b297a3d226a8166f0edc08f89f1
Adean Colon <aecf98@gmail.com>
2023-05-04 19:19:14 -0400

    refactor navbar for analytics

--- frontend/centro-capital-frontend/src/components/CustomNavbar.js
+++ frontend/centro-capital-frontend/src/components/CustomNavbar.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Outlet, Link, useNavigate } from "react-router-dom";
+import { initGA, logPageView } from '../analytics';
 
-class CustomNavbar extends Component {
+const CustomNavbar = () => {
 
     const navigate = useNavigate();
 
@@ -17,32 +18,29 @@ class CustomNavbar extends Component {
        navigate(path);
         logPageView(path);
     };
-
-
-
-    render() {
-        return (
-            <div className='sticky-bottom'>
-                <Navbar bg="light" expand="lg">
-                <img src="favicon.ico" className="img-responsive rounded float-left" width="5%" alt="Logo"></img>
-                    <Container>
-                        <Navbar.Brand onClick={() => handleNavItemClick('/')}>Centro Capital</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link onClick={() => handleNavItemClick('/')}>Home</Nav.Link>
-                                <Nav.Link onClick={() => handleNavItemClick('/insights')}>Insights</Nav.Link>
-                                <Nav.Link onClick={() => handleNavItemClick('/compare')}>Compare</Nav.Link>
-                                <Nav.Link onClick={() => handleNavItemClick('/about')}>About</Nav.Link>
-                                <Nav.Link onClick={() => handleNavItemClick('/feedback')}>Feedback</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-                <Outlet/>
-            </div>
-        );
-    }
+    
+    return (
+        <div className='sticky-bottom'>
+            <Navbar bg="light" expand="lg">
+            <img src="favicon.ico" className="img-responsive rounded float-left" width="5%" alt="Logo"></img>
+                <Container>
+                    <Navbar.Brand onClick={() => handleNavItemClick('/')}>Centro Capital</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link onClick={() => handleNavItemClick('/')}>Home</Nav.Link>
+                            <Nav.Link onClick={() => handleNavItemClick('/insights')}>Insights</Nav.Link>
+                            <Nav.Link onClick={() => handleNavItemClick('/compare')}>Compare</Nav.Link>
+                            <Nav.Link onClick={() => handleNavItemClick('/about')}>About</Nav.Link>
+                            <Nav.Link onClick={() => handleNavItemClick('/feedback')}>Feedback</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Outlet/>
+        </div>
+    );
 }
+
 
 export default CustomNavbar;
