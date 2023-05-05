ebf2b79c6f7008552b726cfdd694cc0b73b5abcb
juandiaz28 <juan.diaz28@upr.edu>
2023-05-05 14:00:11 -0400

    made bar comparison resposnive

--- frontend/centro-capital-frontend/src/components/CustomCompareBar.js
+++ frontend/centro-capital-frontend/src/components/CustomCompareBar.js
@@ -7,6 +7,7 @@ import {
   Tooltip,
   Legend,
   Bar,
+  ResponsiveContainer,
 } from 'recharts';
 
 // Dummy data
@@ -44,6 +45,7 @@ function CustomCompareBar(){
         <button onClick={() => setData(filterData('2017'))}>2017</button>
         <button onClick={() => setData(filterData('2018'))}>2018</button>
       </div>
+      <ResponsiveContainer height={300}>
       <BarChart
         width={500}
         height={300}
@@ -58,6 +60,7 @@ function CustomCompareBar(){
         <Bar dataKey="pv" fill="#8884d8" />
         <Bar dataKey="uv" fill="#82ca9d" />
       </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
